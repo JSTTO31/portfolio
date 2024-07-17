ebb46f5ec5016f0ecca5ae7d4aa906724a6d5516
Joshua Sotto <joshuasottoelyen@gmail.com>
2024-07-18 02:27:10 +0800

    Project section Completed

--- pages/index.vue
+++ pages/index.vue
@@ -2,22 +2,7 @@
     <v-main class="bg-deep-purple-lighten-5">
         <home-section></home-section>
         <service-section></service-section>
-        <v-container class="py-15">
-            <div class="w-100 d-flex justify-center flex-column align-center">
-                <div style="width: 800px" class="">
-                    <h1 class="text-center" style="z-index: 10;">My Journey Through Web Development Excellence</h1>
-                    <div style="height: 12px;width: 100%;z-index: 5"
-                        class="bg-deep-purple-lighten-2 mt-n3 rounded-pill"></div>
-                </div>
-                <p style="font-size: 17px;" class="mt-3 font-weight-medium">Explore my projects, showcasing my journey
-                    as an aspiring full-stack developer.</p>
-            </div>
-            <v-row class="mt-15">
-                <v-col cols="6" v-for="portfolio in portfolios" :key="portfolio.id">
-                    <portfolio-card-item :portfolio="portfolio"></portfolio-card-item>
-                </v-col>
-            </v-row>
-        </v-container>
+        <project-section></project-section>
         <v-dialog fullscreen :model-value="$route.name == 'index-id'" persistent no-click-animation  @click:outside="$router.push({name: 'index'})" >
                 <v-card class="rounded-0">
                     <nuxt-page></nuxt-page>
@@ -28,8 +13,7 @@
 </template>
 
 <script setup lang="ts">
-const portfolios = usePortfolioObject()
-const router = useRouter()
+
 
 
 
