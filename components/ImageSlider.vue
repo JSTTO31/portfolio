ab060e62aac5ae1930c40270e7371bec39d7da0e
Joshua Sotto <joshuasottoelyen@gmail.com>
2024-08-06 14:30:15 +0800

    margin inline

--- components/ImageSlider.vue
+++ components/ImageSlider.vue
@@ -2,22 +2,22 @@
     <div class="w-100">
         <v-card style="overflow: hidden" class="bg-transparent" flat>
             <v-card class="slider-container bg-transparent" flat>
-                <v-icon class="mx-15" size="60" color="grey-darken-1">mdi-vuejs</v-icon>
-                <v-icon class="mx-15" size="60" color="grey-darken-1">mdi-nuxt</v-icon>
-                <v-icon class="mx-15" size="60" color="grey-darken-1">mdi-react</v-icon>
-                <v-icon class="mx-15" size="60" color="grey-darken-1">mdi-laravel</v-icon>
-                <v-icon class="mx-15" size="60" color="grey-darken-1">mdi-nodejs</v-icon>
-                <v-icon class="mx-15" size="60" color="grey-darken-1">mdi-vuetify</v-icon>
-                <v-icon class="mx-15" size="60" color="grey-darken-1">mdi-bootstrap</v-icon>
-                <v-icon class="mx-15" size="60" color="grey-darken-1">mdi-tailwind</v-icon>
-                <v-icon class="mx-15" size="60" color="grey-darken-1">mdi-vuejs</v-icon>
-                <v-icon class="mx-15" size="60" color="grey-darken-1">mdi-nuxt</v-icon>
-                <v-icon class="mx-15" size="60" color="grey-darken-1">mdi-react</v-icon>
-                <v-icon class="mx-15" size="60" color="grey-darken-1">mdi-laravel</v-icon>
-                <v-icon class="mx-15" size="60" color="grey-darken-1">mdi-nodejs</v-icon>
-                <v-icon class="mx-15" size="60" color="grey-darken-1">mdi-vuetify</v-icon>
-                <v-icon class="mx-15" size="60" color="grey-darken-1">mdi-bootstrap</v-icon>
-                <v-icon class="mx-15" size="60" color="grey-darken-1">mdi-tailwind</v-icon>
+                <v-icon class="mx-5 mx-md-15" size="60" color="grey-darken-1">mdi-vuejs</v-icon>
+                <v-icon class="mx-5 mx-md-15" size="60" color="grey-darken-1">mdi-nuxt</v-icon>
+                <v-icon class="mx-5 mx-md-15" size="60" color="grey-darken-1">mdi-react</v-icon>
+                <v-icon class="mx-5 mx-md-15" size="60" color="grey-darken-1">mdi-laravel</v-icon>
+                <v-icon class="mx-5 mx-md-15" size="60" color="grey-darken-1">mdi-nodejs</v-icon>
+                <v-icon class="mx-5 mx-md-15" size="60" color="grey-darken-1">mdi-vuetify</v-icon>
+                <v-icon class="mx-5 mx-md-15" size="60" color="grey-darken-1">mdi-bootstrap</v-icon>
+                <v-icon class="mx-5 mx-md-15" size="60" color="grey-darken-1">mdi-tailwind</v-icon>
+                <v-icon class="mx-5 mx-md-15" size="60" color="grey-darken-1">mdi-vuejs</v-icon>
+                <v-icon class="mx-5 mx-md-15" size="60" color="grey-darken-1">mdi-nuxt</v-icon>
+                <v-icon class="mx-5 mx-md-15" size="60" color="grey-darken-1">mdi-react</v-icon>
+                <v-icon class="mx-5 mx-md-15" size="60" color="grey-darken-1">mdi-laravel</v-icon>
+                <v-icon class="mx-5 mx-md-15" size="60" color="grey-darken-1">mdi-nodejs</v-icon>
+                <v-icon class="mx-5 mx-md-15" size="60" color="grey-darken-1">mdi-vuetify</v-icon>
+                <v-icon class="mx-5 mx-md-15" size="60" color="grey-darken-1">mdi-bootstrap</v-icon>
+                <v-icon class="mx-5 mx-md-15" size="60" color="grey-darken-1">mdi-tailwind</v-icon>
             </v-card>
         </v-card>
     </div>
@@ -35,8 +35,21 @@
     gap: 15px;
     perspective: 100px;
     overflow: visible;
-    animation: scroll 30s linear infinite;
+    animation: scroll 45s linear infinite;
 }
+
+@media only screen and (min-width: 768px)  {
+    .slider-container{
+        overflow-x: scroll;
+        background-color: transparent;
+        display: flex;
+        gap: 15px;
+        perspective: 100px;
+        overflow: visible;
+        animation: scroll 30s linear infinite;
+    }
+}
+
 
 @keyframes scroll {
     0%{
